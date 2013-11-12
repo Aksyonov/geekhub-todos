ecf8da02a3045fec5a6fc81fdf39b4a0b7e6bdb6
Sergey Aksyonov <sergey@aksyonov.pp.ua>
2013-11-12 22:26:47 +0200

    added memory

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 (function(){
   'use strict';
-  function addItem() {
+  function append(text) {
     var el = document.createElement('li');
-    var text = label.value;
-    el.innerHTML = '<input type="checkbox"> <span>' + text + '</span>';
-    list.appendChild(el);
+    el.innerHTML = '<input type="checkbox"> <span></span>';
+    el.querySelector('span').innerText = text;
+    itemList.appendChild(el);
+  }
+  function addItem() {
+    append(label.value);
+    list.push(label.value);
     label.value = '';
+    localStorage['list'] = JSON.stringify(list);
   }
 
-  var list = document.querySelector('#item-list');
+  var itemList = document.querySelector('#item-list');
   var label = document.querySelector('#label');
+  var list = localStorage['list'] ? JSON.parse(localStorage['list']) : [];
+
+  list.forEach(function (el){
+    append(el);
+  });
 
   document.querySelector('#add-item').addEventListener('click', addItem);
   label.addEventListener('keyup', function(event) {
@@ -20,7 +30,9 @@
   document.querySelector('#remove-items').addEventListener('click', function() {
     [].forEach.call(document.querySelectorAll('li input:checked'), function(el) {
       var li = el.parentNode;
-      list.removeChild(li);
+      list.splice(list.indexOf(li.querySelector('span').innerText), 1);
+      itemList.removeChild(li);
+      localStorage['list'] = JSON.stringify(list);
     });
   });
 })();
